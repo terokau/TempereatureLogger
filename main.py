7ca1498fee714b21ca9e047dadebb5a61b9ce413
Tero Kauria <tero.kauria@live.com>
2020-10-15 15:19:25 +0300

    Made measure of cpu,envoirement and ligth

--- main.py
+++ main.py
@@ -4,34 +4,74 @@ import pandas as pd
 import subprocess as sp
 import multiprocessing as mp
 import time
+from datetime import datetime
+import nmap
+import gpiozero as pi
+from omron_2jcie_bu01 import Omron2JCIE_BU01
 
 def main():
 	manager = mp.Manager()
 	
-	a = manager.list()
-	print("start of program")
-	
-	jobs = []
-	for i in range(1,10):
-		p = mp.Process(target=pinger, args=[i,a])
-		jobs.append(p)
-		p.start()
-		p.join()
-		
-	while checkJobs(jobs):
-		time.sleep(1)
-	
-	print(a)
+	getTemperatures = []
+	getSensorTemp = []
+	getLight = []
+	setMeasureMaxLength = 10 #hours
+	setInterVal = 10 #seconds
+	setArrayLength =  setMeasureMaxLength*60*60/setInterVal
 
-def pinger(i,a):
-	address = "192.168.1." + str(i)
-	try :
-		sp.check_output(["ping" , "-c" , "1" , address])
-		print("response from: " , address)
-		a.append(i)
-	except sp.CalledProcessError:
-		print("failed of ping " , address )
+	cpu = pi.CPUTemperature()
+	sensor = Omron2JCIE_BU01.serial("/dev/ttyUSB0")
+	devinfo = sensor.info()
 	
+	
+	print(devinfo)
+	print("start of program ")
+	print("length: " , setMeasureMaxLength,"h , Interval: " , setInterVal, "s array length:" , setArrayLength)
+	plt.ion()
+	while True:
+		data = sensor.latest_data_long()
+		text= str(cpu.temperature) + ";" + str(data.temperature) + ";" + str(data.light)
+		now = datetime.now()
+		text = text+ ";"+ now.strftime("%d/%m/%Y %H:%M:%S")
+		log("tmp.log",text)
+		getTemperatures = controlList(cpu.temperature,getTemperatures,setArrayLength)
+		getSensorTemp = controlList(data.temperature,getSensorTemp,setArrayLength)
+		getLight = controlList(data.light,getLight,setArrayLength)
+		plt.plot(getTemperatures)
+		plt.plot(getSensorTemp)
+		plt.plot(getLight)
+		plt.draw()
+		plt.pause(0.0001)
+		plt.clf()
+		#print(data)
+		#print(getTemperatures)
+
+		time.sleep(10)
+		
+def log(filename,text):
+	f = open(filename, "a")
+	f.write(text+"\r\n")
+	f.close()
+	
+
+	
+def ControlFan(pin,treshold,currentTemperature):
+	fan = pi.LED(pin)
+	if currentTemperature>treshold:
+		print("current temperature:",currentTemperature, " fan: On", fan.pin)
+		fan.on()
+	else:
+		print("current temperature:",currentTemperature, " fan: Off")
+		fan.off()
+	
+
+def controlList(addVal,inList,maxLength):
+	
+	inList.append(addVal)
+	if len(inList) >=maxLength:
+		inList.pop(0)
+	return inList
+
 def checkJobs(jobs):
 	allrun = False
 	for i in jobs:
@@ -40,16 +80,6 @@ def checkJobs(jobs):
 			
 	return allrun
 				
-
-	
-def test(i,a):
-	print("hello from test:" , i)
-	a.append(i)
-
-
-	
-
-
 
 if __name__ == "__main__":
 	main()
